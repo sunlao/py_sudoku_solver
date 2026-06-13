9636f6345946965f7da8b8272c7c5bb08c34afba
Andrew Hansen <sunlao93@gmail.com>
2026-06-13 15:51:47 -0400

    update fixture to use api

--- tests/fixtures/messages.py
+++ tests/fixtures/messages.py
@@ -1,17 +1,8 @@
 import pytest
-from uuid import uuid4
-from datetime import datetime
-from shared.models.constants import MessageTypes
-from shared.models.messages import Message, Metadata, Startup, Board
+from api.v1.messages import start_up
+from shared.models.messages import Message, Board
 
 
 @pytest.fixture
 def startup_message(startup_board: Board) -> Message:
-    """Create a test startup message"""
-    metadata = Metadata(
-        message_id=uuid4(),
-        timestamp=datetime.now(),
-        message_type=MessageTypes.STARTUP,
-    )
-    content = Startup(board=startup_board)
-    return Message(metadata=metadata, content=content)
+    return start_up(startup_board)
