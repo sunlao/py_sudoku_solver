a72672928fc05a8200ba372bef9c729754aa9202
Andrew Hansen <sunlao93@gmail.com>
2026-06-13 11:54:06 -0400

    clean

--- src/actors/state.py
+++ src/actors/state.py
@@ -1,10 +1,20 @@
+from functools import cache
+from shared.models.controller import ProcessStates
+
+
 class State:
+    """Actor state is ephemeral and in-memory for the lifespan of the actor"""
 
     def __init__(self) -> None:
-        pass
+        self.controller_process_state = None
 
-    def set(self) -> None:
-        pass
+    @cache
+    def _cache(self, cache):
+        return cache
 
-    def get(self) -> None:
-        pass
+    def set_controller_process(self, dto: ProcessStates) -> None:
+        self.controller_process_state = dto
+        self._cache(self.controller_process_state)
+
+    def get_controller_process(self) -> ProcessStates:
+        return self._cache(self.controller_process_state)
