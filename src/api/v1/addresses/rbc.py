bf578664ecb716cce440d2f0924963db00a17652
Andrew Hansen <sunlao93@gmail.com>
2026-06-22 21:55:41 -0400

    fixed

--- src/api/v1/addresses/rbc.py
+++ src/api/v1/addresses/rbc.py
@@ -7,7 +7,7 @@ router = APIRouter()
 @router.post("/start/", response_model=None, status_code=status.HTTP_202_ACCEPTED)
 async def start_up(dto: Message) -> None:
     try:
-        print(f"\nstart: {dto.metadata.actor_behavior}\n")
+        print(f"start: {dto.metadata.actor_behavior}\n")
     except Exception as exc:  # pylint: disable=broad-except
         raise HTTPException(
             status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
@@ -18,7 +18,7 @@ async def start_up(dto: Message) -> None:
 @router.post("/cell-update/", response_model=None, status_code=status.HTTP_202_ACCEPTED)
 async def cell_update(dto: Message) -> None:
     try:
-        print(f"\nstart: {dto.metadata.actor_behavior}\n")
+        print(f"cell-update: {dto.metadata.actor_behavior}\n")
     except Exception as exc:  # pylint: disable=broad-except
         raise HTTPException(
             status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
