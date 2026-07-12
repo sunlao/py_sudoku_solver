05a1689f1860c448fc849e0c8ba3c2587981dd15
Andrew Hansen <sunlao93@gmail.com>
2026-07-12 14:39:35 -0400

    update

--- src/actors/actor_tasks/rbc/update.py
+++ src/actors/actor_tasks/rbc/update.py
@@ -20,12 +20,12 @@ class Update:
         rbc_old = side_effects.state.get_cache(dto)
         cell_cnt = [c for c in rbc_old.cells if c.value is None]
         if len(cell_cnt) == 0:
-            print("done")
+            print("NoOp-Already-Completed")
             return
         new_cell = dto.content
-        old_cell = next((c for c in rbc_old.cells if c.id == new_cell.id))
-        if new_cell == old_cell:
-            print("NoOp")
+        old_cell = next(c for c in rbc_old.cells if c.id == new_cell.id)
+        if new_cell == old_cell or old_cell.value is not None:
+            print("NoOp-No-Change")
             return
         rbc_update = rbc_old.model_copy(
             update={
@@ -34,14 +34,20 @@ class Update:
                 )
             }
         )
-        rbc_cells = await self.evaluate.all(side_effects, rbc_update)
-        side_effects.state.set_rbc_cell(dto, rbc_cells)
-        await self.send.rbcs(side_effects, dto, rbc_old, rbc_cells)
-        msg = xform_update_state_msg(
-            sending_actor=actor,
-            sending_status=ActorDomainStatus.WORKING,
-            last_director_timestamp=director_now,
-            rbc_flag=True,
-        )
-        await send_update_msg(side_effects, msg)
-        print(f"**director rbc:update end {dto.metadata.actor_behavior}")
+        if new_cell.value is not None:      
+            side_effects.state.set_rbc_cell(dto, rbc_update)
+            print("NoOp-Completed")
+            return
+        print(f"old_cell: {old_cell}")
+        print(f"new_cell: {new_cell}")
+        # rbc_cells = await self.evaluate.all(side_effects, rbc_update)
+        
+        # await self.send.rbcs(side_effects, dto, rbc_old, rbc_cells)
+        # msg = xform_update_state_msg(
+        #     sending_actor=actor,
+        #     sending_status=ActorDomainStatus.WORKING,
+        #     last_director_timestamp=director_now,
+        #     rbc_flag=True,
+        # )
+        # await send_update_msg(side_effects, msg)
+        # print(f"**director rbc:update end {dto.metadata.actor_behavior}")
