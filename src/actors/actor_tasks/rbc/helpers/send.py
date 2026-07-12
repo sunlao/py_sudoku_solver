2c3d928716fa3ae0ac4c497bb72d03ed0cb85281
Andrew Hansen <sunlao93@gmail.com>
2026-07-12 12:09:29 -0400

    update

--- src/actors/actor_tasks/rbc/helpers/send.py
+++ src/actors/actor_tasks/rbc/helpers/send.py
@@ -1,19 +1,55 @@
+from shared.models.board import CellBehaviorMaps, CellBehaviors
 from shared.models.constants import CellIds
-from shared.models.messages import Message, RBCCells
+from shared.models.messages import Message, RBCCells, Cell, Metadata, ActorBehaviors
 from shared.models.side_effects import ActorSideEffects
-
 
 
 class Send:
 
-    def _updated_cell_ids(self, old: RBCCells, new: RBCCells) -> tuple[CellIds, ...]:
+    def _message(self, actor_behaviors: ActorBehaviors, cell: Cell) -> Message[Cell]:
+        actor, _ = actor_behaviors.split(".", maxsplit=1)
+        m = Metadata(actor_behavior=ActorBehaviors(f"{actor}.update"), rbc_flag=True)
+        return Message(metadata=m, content=cell)
+
+    def _messages(
+        self, maps: CellBehaviorMaps, rbc: RBCCells
+    ) -> tuple[Message[Cell], ...]:
         return tuple(
+            self._message(actor_behavior, cell)
+            for cell_behaviors in maps.maps
+            for cell in rbc.cells
+            if cell.id == cell_behaviors.id
+            for actor_behavior in cell_behaviors.behaviors
+        )
+
+    @staticmethod
+    def _cell_behaviors(id: CellIds, maps: CellBehaviorMaps) -> CellBehaviors:
+        return next((m for m in maps.maps if m.id == id))
+
+    def _cell_behavior_maps(
+        self,
+        cell_ids: frozenset[CellIds],
+        maps: CellBehaviorMaps,
+    ) -> CellBehaviorMaps:
+        return CellBehaviorMaps(
+            maps=tuple(self._cell_behaviors(i, maps) for i in cell_ids)
+        )
+
+    def _updated_cell_ids(self, old: RBCCells, new: RBCCells) -> frozenset[CellIds]:
+        return frozenset(
             new_cell.id
             for old_cell, new_cell in zip(old.cells, new.cells, strict=True)
             if old_cell.value != new_cell.value
             or old_cell.candidates != new_cell.candidates
         )
 
-    def rbcs(self, side_effects: ActorSideEffects, dto: Message[RBCCells], new: RBCCells):
+    async def rbcs(
+        self, side_effects: ActorSideEffects, dto: Message[RBCCells], new: RBCCells
+    ):
+        ids = self._updated_cell_ids(dto.content, new)
         maps = side_effects.static_data(dto).rbc_cell_behavior_maps()
-        updated_cell_ids = self._updated_cell_ids(dto.content, new)
+        cell_behavior_maps = self._cell_behavior_maps(ids, maps)
+        messages = self._messages(cell_behavior_maps, new)
+        await side_effects.gather(
+            *(side_effects.mailbox.enqueue(message) for message in messages)
+        )
---
+++ src/actors/actor_tasks/rbc/update.py
@@ -0,0 +1,30 @@
+from actors.actor_tasks.shared import send_update_msg, xform_update_state_msg
+from actors.actor_tasks.rbc.helpers.evaluate import Evaluate
+from actors.actor_tasks.rbc.helpers.send import Send
+from shared.models.constants import ActorDomainStatus
+from shared.models.messages import Message, RBCCells
+from shared.models.side_effects import ActorSideEffects
+
+
+class Update:
+    def __init__(self) -> None:
+        self.evaluate = Evaluate()
+        self.send = Send()
+
+    async def director(
+        self, side_effects: ActorSideEffects, dto: Message[RBCCells]
+    ) -> None:
+        print(f"**director rbc:update start {dto.metadata.actor_behavior}")
+        # director_now = side_effects.now()
+        # actor, _ = dto.metadata.actor_behavior.split(".", maxsplit=1)
+        # rbc_cells = await self.evaluate.all(side_effects, dto.content)
+        # side_effects.state.set_rbc_cell(dto, rbc_cells)
+        # await self.send.rbcs(side_effects, dto, rbc_cells)
+        # msg = xform_update_state_msg(
+        #     sending_actor=actor,
+        #     sending_status=ActorDomainStatus.WORKING,
+        #     last_director_timestamp=director_now,
+        #     rbc_flag=True,
+        # )
+        # await send_update_msg(side_effects, msg)
+        # print(f"**director rbc:start-up end {dto.metadata.actor_behavior}")
