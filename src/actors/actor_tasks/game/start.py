c3acaa569d79887b8b3bba96d5822294fe7d77a8
Andrew Hansen <sunlao93@gmail.com>
2026-08-24 07:56:33 -0400

    u

--- src/actors/actor_tasks/game/start.py
+++ src/actors/actor_tasks/game/start.py
@@ -11,25 +11,25 @@ class Start:
         self.send = Send()
 
     async def _send_controller(
-            self, 
-            side_effects: ActorSideEffects, 
-            dto: Message[GameStart], 
+            self,
+            side_effects: ActorSideEffects,
+            dto: Message[GameStart],
             director_now: datetime
         ) -> None:
         actor, _ = dto.metadata.actor_behavior.split(".", maxsplit=1)
-        dto = PostControllerUpdate(
-            side_effects=side_effects, 
+        send_dto = PostControllerUpdate(
+            side_effects=side_effects,
             sending_actor=actor,
             sending_status=ActorDomainStatus.STARTED,
             last_director_timestamp=director_now,
             rbc_flag=False
         )
-        await self.send.post_controller_update(dto)
+        await self.send.post_controller_update(send_dto)
 
     async def director(
         self, side_effects: ActorSideEffects, dto: Message[GameStart]
     ) -> None:
         director_now = side_effects.now()
         side_effects.state.set_game_board(dto, dto.content.board)
-        self._send_controller(side_effects, dto, director_now)
+        await self._send_controller(side_effects, dto, director_now)
         print("**director game:start end")
