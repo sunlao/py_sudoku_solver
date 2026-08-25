9ae1b184d4507257fd9929cad754f24dbc1a6d0e
Andrew Hansen <sunlao93@gmail.com>
2026-08-24 20:51:24 -0400

    u

--- src/actors/actor_tasks/game/update.py
+++ src/actors/actor_tasks/game/update.py
@@ -1,3 +1,6 @@
+from shared.models.messages import Cell, Message
+from shared.models.side_effects import ActorSideEffects
+
 class Update:
     def __init__(self) -> None:
         pass
@@ -11,5 +14,6 @@ class Update:
     def send_update_process(self) -> None:
         pass
 
-    def director(self) -> None:
-        pass
+    async def director(
+        self, side_effects: ActorSideEffects, dto: Message[Cell]
+    ) -> None:
