df3fd678b010e46f9815cd71b050a82a833a4c92
Andrew <sunlao93@gmail.com>
2026-08-26 08:32:50 -0400

    Update update.py

--- src/actors/actor_tasks/game/update.py
+++ src/actors/actor_tasks/game/update.py
@@ -1,20 +1,53 @@
-from shared.models.messages import Cell, Message
-from shared.models.side_effects import ActorSideEffects
+from datetime import datetime
+
+from actors.actor_tasks.send import Send
+from shared.models.constants import ActorDomainStatus
+from shared.models.messages import Board, Cell, Message
+from shared.models.side_effects import ActorSideEffects, PostControllerUpdate
+
 
 class Update:
+
     def __init__(self) -> None:
-        pass
+        self.send = Send()
 
-    def update_cell(self) -> None:
-        pass
-
-    def set_board_cell(self) -> None:
-        pass
-
-    def send_update_process(self) -> None:
-        pass
+    async def _send_controller(
+        self,
+        side_effects: ActorSideEffects,
+        dto: Message[Cell],
+        director_now: datetime,
+    ) -> None:
+        actor, _ = dto.metadata.actor_behavior.split(".", maxsplit=1)
+        send_dto = PostControllerUpdate(
+            side_effects=side_effects,
+            sending_actor=actor,
+            sending_status=ActorDomainStatus.DONE,
+            last_director_timestamp=director_now,
+            rbc_flag=False,
+        )
+        await self.send.post_controller_update(send_dto)
 
     async def director(
         self, side_effects: ActorSideEffects, dto: Message[Cell]
     ) -> None:
-        pass
+        director_now = side_effects.now()
+        board: Board = side_effects.state.get_cache(dto)
+        cell = dto.content
+        old_cell = next(c for c in board.cells if c.id == cell.id)
+
+        if old_cell.value is not None:
+            await self._send_controller(side_effects, dto, director_now)
+            print("**game:update NoOp")
+            return None
+
+        board_new = board.model_copy(
+            update={
+                "cells": tuple(
+                    cell if c.id == cell.id else c
+                    for c in board.cells
+                )
+            }
+        )
+        side_effects.state.set_game_board(dto, board_new)
+        await self._send_controller(side_effects, dto, director_now)
+        print("**game:update end")
