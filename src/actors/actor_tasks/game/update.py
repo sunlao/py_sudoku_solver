53e581674bfd1bee6d0aef674d15eeaab1429caf
Andrew <sunlao93@gmail.com>
2026-08-26 08:46:43 -0400

    Update update.py

--- src/actors/actor_tasks/game/update.py
+++ src/actors/actor_tasks/game/update.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from actors.actor_tasks.send import Send
 from shared.models.constants import ActorDomainStatus
 from shared.models.messages import Board, Cell, Message
@@ -11,17 +10,37 @@ class Update:
     def __init__(self) -> None:
         self.send = Send()
 
+    @staticmethod
+    def _new_board(board: Board, cell: Cell) -> Board:
+        return board.model_copy(
+            update={
+                "cells": tuple(
+                    cell if c.id == cell.id else c
+                    for c in board.cells
+                )
+            }
+        )
+
+    @staticmethod
+    def _status(board: Board) -> ActorDomainStatus:
+        return (
+            ActorDomainStatus.WORKING
+            if any(cell.value is None for cell in board.cells)
+            else ActorDomainStatus.DONE
+        )
+
     async def _send_controller(
         self,
         side_effects: ActorSideEffects,
         dto: Message[Cell],
         director_now: datetime,
+        sending_status: ActorDomainStatus,
     ) -> None:
         actor, _ = dto.metadata.actor_behavior.split(".", maxsplit=1)
         send_dto = PostControllerUpdate(
             side_effects=side_effects,
             sending_actor=actor,
-            sending_status=ActorDomainStatus.DONE,
+            sending_status=sending_status,
             last_director_timestamp=director_now,
             rbc_flag=False,
         )
@@ -36,18 +55,15 @@ class Update:
         old_cell = next(c for c in board.cells if c.id == cell.id)
 
         if old_cell.value is not None:
-            await self._send_controller(side_effects, dto, director_now)
+            await self._send_controller(
+                side_effects, dto, director_now, self._status(board)
+            )
             print("**game:update NoOp")
             return None
 
-        board_new = board.model_copy(
-            update={
-                "cells": tuple(
-                    cell if c.id == cell.id else c
-                    for c in board.cells
-                )
-            }
-        )
+        board_new = self._new_board(board, cell)
         side_effects.state.set_game_board(dto, board_new)
-        await self._send_controller(side_effects, dto, director_now)
+        await self._send_controller(
+            side_effects, dto, director_now, self._status(board_new)
+        )
         print("**game:update end")
