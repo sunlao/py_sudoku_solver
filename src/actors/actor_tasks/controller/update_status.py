5d0e347918fad6e15d109fcdb2c4f78277475bf9
Andrew Hansen <sunlao93@gmail.com>
2026-08-27 01:35:33 -0400

    u

--- src/actors/actor_tasks/controller/update_status.py
+++ src/actors/actor_tasks/controller/update_status.py
@@ -1,28 +1,49 @@
 from shared.models.messages import Message
 from shared.models.side_effects import ActorSideEffects
-from shared.models.state import ActorDomainState, ActorDomainStates
+from shared.models.state import ActorDomainState, ActorDomainStates, ActorNames, ActorDomainStatus
 
 
 class UpdateStatus:
-    @staticmethod
-    def _pick_state(
-        new: ActorDomainState, current: ActorDomainState
-    ) -> ActorDomainState:
-        if new.actor == current.actor:
-            return new
-        return current
 
-    def _xform_actor_domain_states(
-        self, ads: ActorDomainStates, new: ActorDomainState
+    @staticmethod
+    def _actor_domain_state(
+        new: ActorDomainState, old: ActorDomainState
+    ) -> ActorDomainState:
+        if new.actor == old.actor:
+            return new
+        return old
+
+    def _actor_domain_states(
+        self, old: ActorDomainStates, new: ActorDomainState
     ) -> ActorDomainStates:
         return ActorDomainStates(
-            states=tuple(self._pick_state(new, s) for s in ads.states)
+            states=tuple(self._actor_domain_state(new, o) for o in old.states)
+        )
+
+    @staticmethod
+    def _game_done(new: ActorDomainStates) -> bool:
+        return any(
+            state.actor == ActorNames.GAME
+            and state.status == ActorDomainStatus.DONE
+            for state in new.states
+        )
+
+    @staticmethod
+    def _rbc_working(new: ActorDomainStates) -> bool:
+        return all(
+            state.status == ActorDomainStatus.WORKING
+            for state in new.states
+            if state.rbc_flag
         )
 
     async def director(
         self, side_effects: ActorSideEffects, dto: Message[ActorDomainState]
     ) -> None:
-        ads = side_effects.state.get_cache(dto)
-        new_ads = self._xform_actor_domain_states(ads, dto.content)
-        side_effects.state.set_domain_actors_state(dto, new_ads)
+        old = side_effects.state.get_cache(dto)
+        new = self._actor_domain_states(old, dto.content)
+        side_effects.state.set_domain_actors_state(dto, new)
+        if self._game_done(new) is True:
+            pass # send
+        if self._rbc_working(new) is False:
+            pass # send
         print("**director controller: update_status end ")
