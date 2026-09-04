ba84dea0d2ecab00833e3623006a9a86f9390db7
Andrew Hansen <sunlao93@gmail.com>
2026-09-03 21:41:18 -0400

    u

--- src/actors/actor_tasks/board/helpers/evaluate.py
+++ src/actors/actor_tasks/board/helpers/evaluate.py
@@ -1,15 +1,14 @@
+from actors.actor_tasks.board.algorithms.color import Color
+from actors.actor_tasks.board.algorithms.fish import Fish
 from actors.actor_tasks.board.algorithms.forcing import Forcing
 from actors.actor_tasks.board.algorithms.intersection import Intersection
 from actors.actor_tasks.board.algorithms.rectangle import Rectangle
-from actors.actor_tasks.board.algorithms.color import Color
-from actors.actor_tasks.board.algorithms.fish import Fish
 from actors.actor_tasks.board.algorithms.xy import XY
 from shared.models.messages import Board, Cell
 from shared.models.side_effects import ActorSideEffects
 
 
 class Evaluate:
-
     def __init__(self) -> None:
         self.color = Color()
         self.fish = Fish()
@@ -45,13 +44,13 @@ class Evaluate:
 
     async def all(self, side_effects: ActorSideEffects, board: Board) -> Board:
         results = await side_effects.gather(
-            side_effects.run_sync(self.fish.fish, board, 2),
-            side_effects.run_sync(self.fish.fish, board, 3),
-            side_effects.run_sync(self.fish.fish, board, 4),
+            side_effects.run_sync(self.fish.x_wing, board),
+            side_effects.run_sync(self.fish.swordfish, board),
+            side_effects.run_sync(self.fish.jellyfish, board),
             side_effects.run_sync(self.xy.y_wing, board),
             side_effects.run_sync(self.xy.xyz_wing, board),
-            side_effects.run_sync(self.color.coloring, board, False),
-            side_effects.run_sync(self.color.coloring, board, True),
+            side_effects.run_sync(self.color.simple, board),
+            side_effects.run_sync(self.color.multi, board),
             side_effects.run_sync(self.xy.xy_chain, board),
             side_effects.run_sync(self.forcing.forcing_chains, board),
             side_effects.run_sync(self.rectangle.unique_rectangle, board),
